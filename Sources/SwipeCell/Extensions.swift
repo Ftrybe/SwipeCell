6d69d10de419f9b51c80b6c6216cd93b888224ca
Ftrybe <fcbyeah@yeah.net>
2021-05-16 14:52:47 +0800

    Update Extensions.swift
    only

--- Sources/SwipeCell/Extensions.swift
+++ Sources/SwipeCell/Extensions.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 public extension View {
     
-    func onSwipe(leading: [Slot] = [], trailing: [Slot] = []) -> some View {
-        return self.modifier(SlidableModifier(leading: leading, trailing: trailing))
+    func onSwipe(leading: [Slot] = [], trailing: [Slot] = [], only: Bool = false) -> some View {
+        return self.modifier(SlidableModifier(leading: leading, trailing: trailing, only: only))
     }
     
     func embedInAnyView() -> AnyView {
